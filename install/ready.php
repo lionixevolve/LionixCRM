7efe4af9cc26aa34f9019cb755081fb7b00f2529
Cristian Umaña <cumana@lionix.com>
2016-06-27 14:42:17 -0600

    docs: Added reference to LionixCRM developed by Lionix Evolve S.A.

--- install/ready.php
+++ install/ready.php
@@ -7,6 +7,9 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
  * Copyright (C) 2011 - 2014 Salesagility Ltd.
  *
+ * LionixCRM is an extension to SuiteCRM developed by Lionix Evolve S.A.
+ * Copyright (C) 2016 Lionix Evolve S.A.
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
  * Free Software Foundation with the addition of the following permission added
@@ -35,7 +38,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
  * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+* display the words "Powered by SugarCRM", "Supercharged by SuiteCRM" and "Evolved by LionixCRM".
  ********************************************************************************/
 
 
