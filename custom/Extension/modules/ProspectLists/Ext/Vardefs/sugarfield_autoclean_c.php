e8ac7913139b011e6c2f019e1e26b4f1515ca8e8
Cristian Umaña <cumana@lionix.com>
2016-07-19 16:34:59 -0600

    fix: labels case

--- custom/Extension/modules/ProspectLists/Ext/Vardefs/sugarfield_autoclean_c.php
+++ custom/Extension/modules/ProspectLists/Ext/Vardefs/sugarfield_autoclean_c.php
@@ -1,6 +1,6 @@
 <?php
  // created: 2016-07-15 12:36:08
 $dictionary['ProspectList']['fields']['autoclean_c']['inline_edit']='1';
-$dictionary['ProspectList']['fields']['autoclean_c']['labelValue']='AUTOCLEAN';
+$dictionary['ProspectList']['fields']['autoclean_c']['labelValue']='Autoclean';
 
  ?>
--- custom/modules/ProspectLists/language/en_us.lang.php
+++ custom/modules/ProspectLists/language/en_us.lang.php
@@ -1,6 +1,6 @@
 <?php
 // created: 2016-07-15 12:36:08
 $mod_strings = array (
-  'LBL_AUTOFILL' => 'AUTOFILL',
-  'LBL_AUTOCLEAN' => 'AUTOCLEAN',
+  'LBL_AUTOFILL' => 'Autofill',
+  'LBL_AUTOCLEAN' => 'Autoclean',
 );
