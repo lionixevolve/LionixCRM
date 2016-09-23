a131ff468e9f28ea51e5b43ab03b8041b573a884
Cristian Umaña <cumanacr@gmail.com>
2016-09-22 18:50:21 -0600

    fix: removed file that broke the installer

--- custom/modules/Cases/logic_hooks_after_retrieve.php
+++ custom/modules/Cases/logic_hooks_after_retrieve.php
@@ -1,6 +1,7 @@
 <?php
 Class LXCasesAfterRetrieveMethods{
 
+//$hook_array['after_retrieve'][] = Array(1, 'setElapsedTime', 'custom/modules/Cases/logic_hooks_after_retrieve.php','LXCasesAfterRetrieveMethods', 'setElapsedTime');
    function setElapsedTime(&$bean, $event, $arguments){
 
       if ($_REQUEST['action'] == "DetailView"){
