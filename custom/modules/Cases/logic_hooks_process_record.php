5d2e7132488ed5f5a1377a07411b1bb5516daa6b
Cristian Umaña <cumanacr@gmail.com>
2016-09-23 21:33:11 -0600

    fix: missing fields on cases listview

--- custom/modules/Cases/logic_hooks_process_record.php
+++ custom/modules/Cases/logic_hooks_process_record.php
@@ -4,8 +4,6 @@ class LXCasesProcessRecordMethods
 {
     public function setElapsedTime(&$bean, $event, $arguments)
     {
-        if ($_REQUEST['action'] == 'DetailView') {
-            $bean->elapsedtime = lxHumanReadableElapsedTime($bean->elapsedtimeinmins_c, $GLOBALS['current_language'], 2);
-        }
+        $bean->elapsedtime = lxHumanReadableElapsedTime($bean->elapsedtimeinmins_c, $GLOBALS['current_language'], 2);
     }
 }
---
+++ custom/modules/Cases/metadata/listviewdefs.php
@@ -0,0 +1,73 @@
+<?php
+$listViewDefs ['Cases'] =
+array (
+  'CASE_NUMBER' =>
+  array (
+    'width' => '5%',
+    'label' => 'LBL_LIST_NUMBER',
+    'default' => true,
+  ),
+  'NAME' =>
+  array (
+    'width' => '25%',
+    'label' => 'LBL_LIST_SUBJECT',
+    'link' => true,
+    'default' => true,
+  ),
+  'ACCOUNT_NAME' =>
+  array (
+    'width' => '20%',
+    'label' => 'LBL_LIST_ACCOUNT_NAME',
+    'module' => 'Accounts',
+    'id' => 'ACCOUNT_ID',
+    'link' => true,
+    'default' => true,
+    'ACLTag' => 'ACCOUNT',
+    'related_fields' =>
+    array (
+      0 => 'account_id',
+    ),
+  ),
+  'PRIORITY' =>
+  array (
+    'width' => '10%',
+    'label' => 'LBL_LIST_PRIORITY',
+    'default' => true,
+  ),
+  'STATUS' =>
+  array (
+    'width' => '10%',
+    'label' => 'LBL_LIST_STATUS',
+    'default' => true,
+  ),
+  'ELAPSEDTIME' =>
+  array (
+    'type' => 'text',
+    'label' => 'elapsedtime',
+    'sortable' => false,
+    'width' => '10%',
+    'default' => true,
+  ),
+  'ELAPSEDTIMEINMINS_C' =>
+  array (
+    'type' => 'int',
+    'default' => true,
+    'label' => 'LBL_ELAPSEDTIMEINMINS',
+    'width' => '10%',
+  ),
+  'ASSIGNED_USER_NAME' =>
+  array (
+    'width' => '10%',
+    'label' => 'LBL_ASSIGNED_TO_NAME',
+    'module' => 'Employees',
+    'id' => 'ASSIGNED_USER_ID',
+    'default' => true,
+  ),
+  'DATE_ENTERED' =>
+  array (
+    'width' => '10%',
+    'label' => 'LBL_DATE_ENTERED',
+    'default' => true,
+  ),
+);
+?>
