3facb246605c73b634463f452e9e5fd3929d33d4
Cristian Umaña <cumanacr@gmail.com>
2016-09-23 18:37:07 -0600

    fix: missing levels depth

--- custom/modules/Cases/logic_hooks_process_record.php
+++ custom/modules/Cases/logic_hooks_process_record.php
@@ -5,7 +5,7 @@ class LXCasesProcessRecordMethods
     public function setElapsedTime(&$bean, $event, $arguments)
     {
         if ($_REQUEST['action'] == 'DetailView') {
-            $bean->elapsedtime = lxHumanReadableElapsedTime($bean->elapsedtimeinmins_c, $GLOBALS['current_language']);
+            $bean->elapsedtime = lxHumanReadableElapsedTime($bean->elapsedtimeinmins_c, $GLOBALS['current_language'], 2);
         }
     }
 }
