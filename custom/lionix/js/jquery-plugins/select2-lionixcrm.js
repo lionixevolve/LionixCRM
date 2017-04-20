dd8371ad811daf68e145dbf55ee63bf1b6cfe58e
Cristian Umaña <cumanacr@gmail.com>
2017-04-20 12:33:20 -0600

    feat: select2 disabled on Administration modules

--- custom/lionix/js/jquery-plugins/select2-lionixcrm.js
+++ custom/lionix/js/jquery-plugins/select2-lionixcrm.js
@@ -4,7 +4,7 @@
 // (function(){})(); // like above but more parens.
 // (function(){}()); // Douglas Crockford's style when you need function results.
 // Further reading: http://javascript.crockford.com/code.html then search for invoked immediately
-! function() {
+!(function() {
     // create an observer instance
     // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
     var observer = new MutationObserver(function(mutations) {
@@ -13,18 +13,26 @@
                 if (!$(this).hasClass("select2-hidden-accessible")) {
                     $(this).select2({
                         closeOnSelect: false,
-                        dropdownAutoWidth: 'true',
-                        width: 'auto', //mucho más ancho
+                        dropdownAutoWidth: "true",
+                        width: "auto", //mucho más ancho
                         theme: "bootstrap",
-                        dropdownParent: $(this).closest('div')
-                    })
+                        dropdownParent: $(this).closest("div")
+                    });
                 }
             });
-            var crmDetailView = document.forms['DetailView'];
+            // select2 disabled for specific view
+            var crmDetailView = document.forms["DetailView"];
             if (crmDetailView) {
-                if (crmDetailView.module.value == 'AOW_WorkFlow') {
-                    $('select').select2('destroy');
+                if (crmDetailView.module.value == "AOW_WorkFlow") {
+                    $("select").select2("destroy");
                 }
+            }
+            // select2 disabled for a whole module
+            if (
+                /module=ModuleBuilder/.test(window.location.search) ||
+                /module=Administration/.test(window.location.search)
+            ) {
+                $("select").select2("destroy");
             }
             // if needed only once, you can stop observing with observer.disconnect();
             // observer.disconnect();
@@ -43,20 +51,20 @@
         // });
     });
     // Observer target
-    var target = document.querySelector('body'); //uncomment to run
+    var target = document.querySelector("body"); //uncomment to run
 
     // configuration of the observer:
     // NOTE: At the very least, childList, attributes, or characterData must be set to true. Otherwise, "An invalid or illegal string was specified" error is thrown.
     var config = {
         attributes: true,
-        childList: true,
+        childList: true
         // characterData: true,
         // subtree: true
     };
     // pass in the target node, as well as the observer options
     observer.observe(target, config); // uncomment to run
     // end observer
-}();
+})();
 
 // function definitions section
 //eof
