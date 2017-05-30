f4eab546c60a998e62b40c64f686bab9a0a93f20
Cristian Umaña <cumanacr@gmail.com>
2017-05-30 17:24:52 -0600

    fix: some fields wasn't hidden correctly

--- custom/lionix/js/miscellaneous/lx-change-visibility-crm-field.js
+++ custom/lionix/js/miscellaneous/lx-change-visibility-crm-field.js
@@ -3,11 +3,13 @@ function lxChangeVisibilityCRMfield(field_name, display, fnCallerName = "") {
     label_field_name_without_c = field_name.replace(/_c$/,'');
     if (display) {
         console.log("lxChangeVisibilityCRMfield showing field " + field_name, fnCallerName);
+        $("#" + field_name).css("visibility", "visible");
         $("#" + field_name).parent("div").css("visibility", "visible");
         $(".label[data-label='LBL_" + field_name.toUpperCase() + "']").css("visibility", "visible");
         $(".label[data-label='LBL_" + label_field_name_without_c.toUpperCase() + "']").css("visibility", "visible");
     } else {
         console.log("lxChangeVisibilityCRMfield hidding field " + field_name, fnCallerName);
+        $("#" + field_name).css("visibility", "hidden");
         $("#" + field_name).parent("div").css("visibility", "hidden");
         $(".label[data-label='LBL_" + field_name.toUpperCase() + "']").css("visibility", "hidden");
         $(".label[data-label='LBL_" + label_field_name_without_c.toUpperCase() + "']").css("visibility", "hidden");
