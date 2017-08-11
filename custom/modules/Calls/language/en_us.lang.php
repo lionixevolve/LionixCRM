399bf74626e89e8427599333cabfc123d0ecfbab
Cristian Umaña <cumanacr@gmail.com>
2017-08-11 13:05:36 -0600

    fix: typo

--- custom/modules/Calls/language/en_us.lang.php
+++ custom/modules/Calls/language/en_us.lang.php
@@ -1,15 +1,15 @@
 <?php
 // created: 2016-09-21 18:59:34
 $mod_strings = array (
-  'LBL_LXTID' => 'Tópico - ID';
+  'LBL_LXTID' => 'Tópico - ID',
   'LBL_LXTESCALATION' => 'Tópico - Escalar';
-  'LBL_LXTNAME' => 'Tópico - Nombre';
-  'LBL_LXTLEVEL2' => 'Tópico - Nivel 2';
-  'LBL_LXTLEVEL3' => 'Tópico - Nivel 3';
-  'LBL_LXTLEVEL4' => 'Tópico - Nivel 4';
-  'LBL_LXTLEVEL5' => 'Tópico - Nivel 5';
-  'LBL_LXTLEVEL6' => 'Tópico - Nivel 6';
-  'LBL_LXTLEVEL7' => 'Tópico - Nivel 7';
+  'LBL_LXTNAME' => 'Tópico - Nombre',
+  'LBL_LXTLEVEL2' => 'Tópico - Nivel 2',
+  'LBL_LXTLEVEL3' => 'Tópico - Nivel 3',
+  'LBL_LXTLEVEL4' => 'Tópico - Nivel 4',
+  'LBL_LXTLEVEL5' => 'Tópico - Nivel 5',
+  'LBL_LXTLEVEL6' => 'Tópico - Nivel 6',
+  'LBL_LXTLEVEL7' => 'Tópico - Nivel 7',
   'LBL_LXTPROCEDURE1' => 'Tópico - Procedimiento 1',
   'LBL_LXTPROCEDURE2' => 'Tópico - Procedimiento 2',
   'LBL_LXTPROCEDURE3' => 'Tópico - Procedimiento 3',
