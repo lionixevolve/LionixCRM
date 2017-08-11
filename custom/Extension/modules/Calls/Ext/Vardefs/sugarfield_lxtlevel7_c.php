2cdee73c96f6700d0605522a0105c805d9da6350
Cristian Umaña <cumanacr@gmail.com>
2017-08-11 12:09:04 -0600

    fix: missing single quote

--- custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_lxtlevel7_c.php
+++ custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_lxtlevel7_c.php
@@ -1,6 +1,6 @@
 <?php
  // created: 2017-07-13 16:27:46
 $dictionary['Call']['fields']['lxtlevel7_c']['inline_edit']='';
-$dictionary['Call']['fields']['lxtlevel7_c']['labelValue']='Nivel 7;
+$dictionary['Call']['fields']['lxtlevel7_c']['labelValue']='Nivel 7';
 
  ?>
