2ffd9905e73edac4d2a21c904d33a4c869c0aa8a
Cristian Umaña <cumanacr@gmail.com>
2017-08-14 18:48:10 -0600

    refactor: better aow_workflow naming

--- custom/lionix/query/install_scripts/install_routines.sql
+++ custom/lionix/query/install_scripts/install_routines.sql
@@ -267,7 +267,7 @@ VALUES (CONCAT(@year,'-12-25'), CONCAT(@year,'-12-25'), DATE_FORMAT(CONCAT(@year
 ##### workflows
 INSERT INTO aow_workflow
 (id, name, date_entered, date_modified, modified_user_id, created_by, description, deleted, assigned_user_id, flow_module, flow_run_on, status, run_when, multiple_runs)
-VALUES('wf-op-reset-date-closed', 'WF-OP-RESET-DATE_CLOSED_NOTIFIED_C', utc_timestamp(), utc_timestamp(), '1', '1', 'Mark dateclosednotified_c as not notified', 0, '1', 'Opportunities', 'All_Records', 'Active', 'On_Save', 1);
+VALUES('wf-op-reset-date-closed', 'WF-OP-NOT-DATE_CLOSED: Reset date_closed_notified_c', utc_timestamp(), utc_timestamp(), '1', '1', 'Mark dateclosednotified_c as not notified', 0, '1', 'Opportunities', 'All_Records', 'Active', 'On_Save', 1);
 
 INSERT INTO aow_conditions
 (id, name, date_entered, date_modified, modified_user_id, created_by, description, deleted, aow_workflow_id, condition_order, module_path, field, operator, value_type, value)
@@ -279,7 +279,7 @@ VALUES('wf-op-reset-date-closed-action-1', 'Enable future notifications for date
 
 INSERT INTO aow_workflow
 (id, name, date_entered, date_modified, modified_user_id, created_by, description, deleted, assigned_user_id, flow_module, flow_run_on, status, run_when, multiple_runs)
-VALUES('wf-op-not-date-closed', 'WF-OP-NOT-DATE_CLOSED: 3 days before', utc_timestamp(), utc_timestamp(), '1', '1', 'Send an email to assigned_user_id and then mark dateclosednotified_c as notified.', 0, '1', 'Opportunities', 'All_Records', 'Active', 'In_Scheduler', 1);
+VALUES('wf-op-not-date-closed', 'WF-OP-NOT-DATE_CLOSED: Notify 3 days before', utc_timestamp(), utc_timestamp(), '1', '1', 'Send an email to assigned_user_id and then mark dateclosednotified_c as notified.', 0, '1', 'Opportunities', 'All_Records', 'Active', 'In_Scheduler', 1);
 
 INSERT INTO aow_conditions
 (id, name, date_entered, date_modified, modified_user_id, created_by, description, deleted, aow_workflow_id, condition_order, module_path, field, operator, value_type, value)
