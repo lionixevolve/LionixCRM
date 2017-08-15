5384ba7a9bd9a1448ebea779a0d22c0ee23f8006
Cristian Umaña <cumanacr@gmail.com>
2017-08-14 20:30:36 -0600

    fix: bad query call

--- custom/modules/Opportunities/logic_hooks_after_retrieve.php
+++ custom/modules/Opportunities/logic_hooks_after_retrieve.php
@@ -9,13 +9,12 @@ class LXOpportunitiesAfterRetrieveMethods
             $query = "
                 select trim(concat(ifnull(first_name,''), ' ',ifnull(last_name,''))) AS 'maincontact_c'
                 from contacts
-                where nivel_precio = '{$bean->maincontact_c}'
+                where id = '{$bean->maincontact_c}'
             ";
-            $results = $db->query($query);
-            if ($row = $db->fetchByAssoc($results)) {
+            $results = $bean->db->query($query);
+            if ($row = $bean->db->fetchByAssoc($results)) {
                 $bean->maincontact_c = mb_convert_encoding($row['maincontact_c'], 'UTF-8');
             }
         }
     }
-
 }
