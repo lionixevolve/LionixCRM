b0b59a20e760df9ddedf4c46c19278e7688066eb
Cristian Umaña <cumanacr@gmail.com>
2017-08-16 16:49:22 -0600

    fix: spanish values

--- custom/include/language/es_ES.lang.php
+++ custom/include/language/es_ES.lang.php
@@ -18,16 +18,16 @@ $GLOBALS['app_list_strings']['sales_probability_dom'] = array(
     'Closed Lost' => '0',
 );
 $GLOBALS['app_list_strings']['sales_stage_dom_oldvalues'] = array(
-    'Prospecting' => 'Prospecting',
-    'Qualification' => 'Qualification',
-    'Needs Analysis' => 'Needs Analysis',
-    'Value Proposition' => 'Value Proposition',
-    'Id. Decision Makers' => 'Identifying Decision Makers',
-    'Perception Analysis' => 'Perception Analysis',
-    'Proposal/Price Quote' => 'Proposal/Price Quote',
-    'Negotiation/Review' => 'Negotiation/Review',
-    'Closed Won' => 'Closed Won',
-    'Closed Lost' => 'Closed Lost',
+    'Prospecting' => 'Prospecto',
+    'Qualification' => 'Calificación',
+    'Needs Analysis' => 'Necesita Análisis',
+    'Value Proposition' => 'Propuesta de Valor',
+    'Id. Decision Makers' => 'Identificar a los tomadores de decisión',
+    'Perception Analysis' => 'Análisis de Percepción',
+    'Proposal/Price Quote' => 'Propuesta/Presupuesto',
+    'Negotiation/Review' => 'Negociación/Revisión',
+    'Closed Won' => 'Ganado',
+    'Closed Lost' => 'Perdido',
 );
 $GLOBALS['app_list_strings']['sales_stage_dom_goods'] = array(
 );
