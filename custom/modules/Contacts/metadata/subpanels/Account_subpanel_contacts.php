6ac3d62fb71063936866a5c507c1f4994c7f98e0
Cristian Umaña <cumanacr@gmail.com>
2017-08-19 18:32:55 -0600

    feat: Account_subpanel_contacts

---
+++ custom/modules/Contacts/metadata/subpanels/Account_subpanel_contacts.php
@@ -0,0 +1,81 @@
+<?php
+// created: 2017-08-19 18:31:46
+$subpanel_layout['list_fields'] = array (
+  'name' => 
+  array (
+    'name' => 'name',
+    'vname' => 'LBL_LIST_NAME',
+    'widget_class' => 'SubPanelDetailViewLink',
+    'module' => 'Contacts',
+    'width' => '43%',
+    'default' => true,
+  ),
+  'lastname2_c' => 
+  array (
+    'type' => 'varchar',
+    'default' => true,
+    'vname' => 'LBL_LASTNAME2',
+    'width' => '10%',
+  ),
+  'primary_address_city' => 
+  array (
+    'name' => 'primary_address_city',
+    'vname' => 'LBL_LIST_CITY',
+    'width' => '20%',
+    'default' => true,
+  ),
+  'primary_address_state' => 
+  array (
+    'name' => 'primary_address_state',
+    'vname' => 'LBL_LIST_STATE',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'email1' => 
+  array (
+    'name' => 'email1',
+    'vname' => 'LBL_LIST_EMAIL',
+    'widget_class' => 'SubPanelEmailLink',
+    'width' => '30%',
+    'sortable' => false,
+    'default' => true,
+  ),
+  'phone_work' => 
+  array (
+    'name' => 'phone_work',
+    'vname' => 'LBL_LIST_PHONE',
+    'width' => '15%',
+    'default' => true,
+  ),
+  'edit_button' => 
+  array (
+    'vname' => 'LBL_EDIT_BUTTON',
+    'widget_class' => 'SubPanelEditButton',
+    'module' => 'Contacts',
+    'width' => '5%',
+    'default' => true,
+  ),
+  'remove_button' => 
+  array (
+    'vname' => 'LBL_REMOVE',
+    'widget_class' => 'SubPanelRemoveButton',
+    'module' => 'Contacts',
+    'width' => '5%',
+    'default' => true,
+  ),
+  'first_name' => 
+  array (
+    'name' => 'first_name',
+    'usage' => 'query_only',
+  ),
+  'last_name' => 
+  array (
+    'name' => 'last_name',
+    'usage' => 'query_only',
+  ),
+  'salutation' => 
+  array (
+    'name' => 'salutation',
+    'usage' => 'query_only',
+  ),
+);
