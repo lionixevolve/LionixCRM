dca1e56d46fa622243a199735befb13ac9d95beb
Matías Barletta <mrb@lionix.com>
2017-08-21 18:07:28 -0600

    style: add show/hide effect for new contacts

--- custom/lionix/js/miscellaneous/lx-show-crm-field.js
+++ custom/lionix/js/miscellaneous/lx-show-crm-field.js
@@ -2,9 +2,9 @@ function lxShowCRMfield(field_name, show, fnCallerName = "") {
     fnCallerName = fnCallerName != "" ? "(Function " + fnCallerName + ")" : "";
     if (show) {
         console.log("lxShowCRMfield showing field " + field_name, fnCallerName);
-        $("#" + field_name).parent('div').parent('div').show();
+        $("#" + field_name).parent('div').parent('div').show(1000);
     } else {
         console.log("lxShowCRMfield hidding field " + field_name, fnCallerName);
-        $("#" + field_name).parent('div').parent('div').hide();
+        $("#" + field_name).parent('div').parent('div').hide(250);
     }
 }
