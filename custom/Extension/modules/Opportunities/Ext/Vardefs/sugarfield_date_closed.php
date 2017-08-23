6b38ed925b2f72b60700ba84750f1bbf4d5fe79a
Cristian Umaña <cumanacr@gmail.com>
2017-08-23 14:03:10 -0600

    fix: date_closed behaviour

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_date_closed.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_date_closed.php
@@ -1,6 +1,6 @@
 <?php
  // created: 2017-08-18 19:29:59
-$dictionary['Opportunity']['fields']['date_closed']['display_default']='+1 week';
+$dictionary['Opportunity']['fields']['date_closed']['display_default']='+1 month';
 $dictionary['Opportunity']['fields']['date_closed']['inline_edit']=true;
 $dictionary['Opportunity']['fields']['date_closed']['comments']='Expected or actual date the oppportunity will close';
 $dictionary['Opportunity']['fields']['date_closed']['merge_filter']='disabled';
