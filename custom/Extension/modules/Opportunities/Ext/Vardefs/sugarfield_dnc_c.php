03c84833b9385aff7b16e444a557d7ac5cdf9fc0
Cristian Umaña <cumanacr@gmail.com>
2017-08-24 14:04:21 -0600

    fix: inline_edit

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_dnc_c.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_dnc_c.php
@@ -1,7 +1,8 @@
 <?php
  // created: 2017-08-18 19:29:59
+$dictionary['Opportunity']['fields']['dnc_c']['inline_edit']='1';
 $dictionary['Opportunity']['fields']['dnc_c']['display_default']='+1 week';
-$dictionary['Opportunity']['fields']['dnc_c']['inline_edit']=true;
+$dictionary['Opportunity']['fields']['dnc_c']['labelValue']='Fecha Próximo Contacto';
 $dictionary['Opportunity']['fields']['dnc_c']['comments']='Expected date next call or contact';
 $dictionary['Opportunity']['fields']['dnc_c']['merge_filter']='disabled';
 
