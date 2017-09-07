f91736f7f1eaf6469bff23a471999b55cfbb8286
Cristian Umaña <cumanacr@gmail.com>
2017-09-06 18:28:52 -0600

    refactor: Hidding UnifiedSearch on top bar

--- custom/lionix/js/miscellaneous/lx-hide-global-search-while-not-working.js
+++ custom/lionix/js/miscellaneous/lx-hide-global-search-while-not-working.js
@@ -1,1 +1,49 @@
-$('.desktop-bar > ul > li > form[name=UnifiedSearch]').hide();
+// This file containts all modules bussines logic
+//Self-Invoking Anonymous Function Notation
+// !function(){}(); // easy to read, the result is unimportant.
+// (function(){})(); // like above but more parens.
+// (function(){}()); // Douglas Crockford's style when you need function results.
+// Further reading: http://javascript.crockford.com/code.html then search for invoked immediately
+!function() {
+    // create an observer instance
+    // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
+    $('.desktop-bar > ul > li > form[name=UnifiedSearch]').hide();
+    var observer = new MutationObserver(function(mutations) {
+        if (mutations) {
+            if ($('.desktop-bar > ul > li > form[name=UnifiedSearch]').is(':visible')) {
+                console.groupCollapsed("Hidding UnifiedSearch '%s' '%s' '%s' '%s'", 'all modules', 'lx-hide-global-search-while-not-working.js', '!function()', 'observer');
+                console.groupEnd();
+                $('.desktop-bar > ul > li > form[name=UnifiedSearch]').hide();
+            }
+            // if needed only once, you can stop observing with observer.disconnect();
+            // observer.disconnect();
+        }
+        // When need to find something special
+        // mutations.forEach(function(mutation) {
+        //     console.log('select2-lionixcrm mutation:', mutation);
+        //     //     if (mutation.type == "attributes") {
+        //     //         if (mutation.target.nodeName == "FORM" && mutation.target.id == "some-form-id") {
+        //     //             //when found do your code
+        //     //             //your code
+        //     //             // if needed only once, you can stop observing with observer.disconnect();
+        //     //             //observer.disconnect();
+        //     //         }
+        //     //     }
+        // });
+    });
+    // Observer target
+    var target = document.querySelector('#content');
+    // configuration of the observer:
+    // NOTE: At the very least, childList, attributes, or characterData must be set to true. Otherwise, "An invalid or illegal string was specified" error is thrown.
+    var config = {
+        attributes: true,
+        childList: true,
+        // characterData: true,
+        subtree: true
+    };
+    // pass in the target node, as well as the observer options
+    observer.observe(target, config);
+    // end observer
+}();
+
+// function definitions section
