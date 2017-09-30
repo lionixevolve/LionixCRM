981f5606f2920346b0ed21348d6827430f79b47f
Cristian Umaña <cumanacr@gmail.com>
2017-09-29 22:21:13 -0600

    refactor: lxShowCRMfield changed to lx.field.show

--- custom/lionix/js/miscellaneous/lx-show-crm-field.js
+++ custom/lionix/js/miscellaneous/lx-show-crm-field.js
@@ -1,10 +1,12 @@
-function lxShowCRMfield(field_name, show, fnCallerName = "") {
-    fnCallerName = fnCallerName != "" ? "(Function " + fnCallerName + ")" : "";
+lx.field.show = function(field_name, show, fnCallerName = "") {
+    fnCallerName = fnCallerName != ""
+        ? "(Function " + fnCallerName + ")"
+        : "";
     if (show) {
-        console.log("lxShowCRMfield showing field " + field_name, fnCallerName);
+        console.log("lx.field.show showing field " + field_name, fnCallerName);
         $("#" + field_name).parent('div').parent('div').show(1000);
     } else {
-        console.log("lxShowCRMfield hidding field " + field_name, fnCallerName);
+        console.log("lx.field.show hidding field " + field_name, fnCallerName);
         $("#" + field_name).parent('div').parent('div').hide(250);
     }
 }
