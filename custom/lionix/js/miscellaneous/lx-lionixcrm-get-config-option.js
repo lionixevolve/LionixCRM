be71b85dfb129e4358c9040aa0fac4bb0f26dbf2
Cristian Umaña <cumanacr@gmail.com>
2017-10-03 20:02:59 -0600

    feat: return data on resolve

--- custom/lionix/js/miscellaneous/lx-lionixcrm-get-config-option.js
+++ custom/lionix/js/miscellaneous/lx-lionixcrm-get-config-option.js
@@ -9,7 +9,7 @@ lx.lionixCRM.getConfigOption = function(option) {
         $.ajax({
             // beforeSend is a pre-request callback function that can be used to modify the jqXHR.
             beforeSend: function(jqXHR, settings) {
-                console.groupCollapsed("Bussines logic '%s' '%s' '%s' '%s'", 'all modules', 'lx-lionixcrm-get-config-option.js', 'lx.lionixCRM.getConfigOption()', 'ajax beforeSend');
+                console.groupCollapsed("Bussines logic '%s' '%s' '%s' option '%s' '%s'", 'all modules', 'lx-lionixcrm-get-config-option.js', 'lx.lionixCRM.getConfigOption()', option, 'ajax beforeSend');
                 console.log("*** start ***");
                 console.log("beforeSend callback:", settings.url);
                 console.groupEnd();
@@ -19,29 +19,30 @@ lx.lionixCRM.getConfigOption = function(option) {
             data: data,
             // success is a function to be called if the request succeeds.
             success: function(data, status, jqXHR) {
-                console.groupCollapsed("Bussines logic '%s' '%s' '%s' '%s'", 'all modules', 'lx-lionixcrm-get-config-option.js', 'lx.lionixCRM.getConfigOption()', 'ajax success');
+                console.groupCollapsed("Bussines logic '%s' '%s' '%s' option '%s' '%s'", 'all modules', 'lx-lionixcrm-get-config-option.js', 'lx.lionixCRM.getConfigOption()', option, 'ajax success');
                 console.log("success callback:", status);
                 console.log("data:", data);
                 if (data == '') {
                     console.log("LionixCRM option " + option + " not found.")
                 } else {
+                    data = JSON.parse(data);
                     if (option == 'all') {
-                        lx.lionixCRM.config = JSON.parse(data);
+                        lx.lionixCRM.config = data;
                     } else {
-                        lx.lionixCRM.config[option] = JSON.parse(data);
+                        lx.lionixCRM.config[option] = data;
                     }
                     console.log("LionixCRM option (" + option + ") updated.")
                 }
                 console.groupEnd();
-                resolve();
+                resolve(data);
             }, // end success
             // error is a function to be called if the request fails.
             error: function(jqXHR, status, error) {
-                console.groupCollapsed("Bussines logic '%s' '%s' '%s' '%s'", 'all modules', 'lx-lionixcrm-get-config-option.js', 'lx.lionixCRM.getConfigOption()', 'ajax error');
+                console.groupCollapsed("Bussines logic '%s' '%s' '%s' option '%s' '%s'", 'all modules', 'lx-lionixcrm-get-config-option.js', 'lx.lionixCRM.getConfigOption()', option, 'ajax error');
                 console.log("error callback:", status);
                 console.log("Function lx.lionixCRM.getConfigOption error:", error);
                 console.groupEnd();
-                reject();
+                reject(error);
             }, // end error
             // complete is a function to be called when the request finishes (after success and error callbacks are executed).
             // complete: function(jqXHR, status) {
