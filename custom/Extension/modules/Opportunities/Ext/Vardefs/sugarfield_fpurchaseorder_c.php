8f05f2d4497bb07020d3ee79841d3428ef032f24
Cristian Umaña <cumanacr@gmail.com>
2017-10-04 12:34:11 -0600

    feat: fpurchaseorder_c field added to opportunities

---
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_fpurchaseorder_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2017-09-30 00:26:22
+$dictionary['Opportunity']['fields']['fpurchaseorder_c']['inline_edit']='';
+$dictionary['Opportunity']['fields']['fpurchaseorder_c']['labelValue']='Orden de Compra';
+
+ ?>
