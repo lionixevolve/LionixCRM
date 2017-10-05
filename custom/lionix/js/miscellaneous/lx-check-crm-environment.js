6ba5c87aeb946ec9333dbd4908b61c585353eab4
Cristian Umaña <cumanacr@gmail.com>
2017-10-04 19:05:04 -0600

    refactor: fewer functions needed

--- custom/lionix/js/miscellaneous/lx-check-crm-environment.js
+++ custom/lionix/js/miscellaneous/lx-check-crm-environment.js
@@ -1,30 +1,22 @@
 // function definitions section
 lx.lionixCRM.getEnvironment = function() {
-    if ($("#LionixCRM-environment").length == 0) {
-        if (lx.lionixCRM.config.environment.toLowerCase() === 'testing') {
-            let pbclass = "progress-bar bg-info";
-            let probability = 100;
-            let text_bar = "Testing Environment"
-            $('#content').before('<div id="LionixCRM-environment" data-enviroment="Testing Environment" class="progresslx"> <div class="' + pbclass + '" role="progressbar" aria-valuenow="' + probability + '" aria-valuemin="0" aria-valuemax="100" style="min-width: 10%; width: ' + probability + '%; height: 30px; font-size:1.9em; line-height: 2.8rem;">' + text_bar + '</div></div>');
+    lx.lionixCRM.getConfigOption('environment').then(function(data) {
+        console.log("environment:", data);
+        if ($("#LionixCRM-environment").length == 0) {
+            if (lx.lionixCRM.config.environment.toLowerCase() === 'testing') {
+                let pbclass = "progress-bar bg-info";
+                let probability = 100;
+                let text_bar = "Testing Environment"
+                $('#content').before('<div id="LionixCRM-environment" data-enviroment="Testing Environment" class="progresslx"> <div class="' + pbclass + '" role="progressbar" aria-valuenow="' + probability + '" aria-valuemin="0" aria-valuemax="100" style="min-width: 10%; width: ' + probability + '%; height: 30px; font-size:1.9em; line-height: 2.8rem;">' + text_bar + '</div></div>');
+            } else {
+                $('#content').before('<div id="LionixCRM-environment" data-enviroment="Production Environment" />');
+            }
+            console.log("LionixCRM-environment added.");
         } else {
-            $('#content').before('<div id="LionixCRM-environment" data-enviroment="Production Environment" />');
+            console.log("LionixCRM-environment exists.");
         }
-        console.log("LionixCRM-environment added.");
-    } else {
-        console.log("LionixCRM-environment exists.");
-    }
+    });
 } // end function
-
-lx.lionixCRM.checkEnvironment = function() {
-    if (lx.lionixCRM.config.environment == undefined) {
-        lx.lionixCRM.getConfigOption('environment').then(function() {
-            lx.lionixCRM.getEnvironment()
-        });
-    } else {
-        lx.lionixCRM.getEnvironment();
-    }
-} // end function
-//eof
 
 // This file containts all modules bussines logic
 //Self-Invoking Anonymous Function Notation
@@ -37,27 +29,12 @@ lx.lionixCRM.checkEnvironment = function() {
     // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
     var observer = new MutationObserver(function(mutations) {
         if (mutations) {
-            if ($("#LionixCRM-environment").length == 0) {
-                console.groupCollapsed("Check environment '%s' '%s' '%s' '%s'", 'all modules', 'lx-check-crm-environment.js', '!function()', 'observer');
-                console.log('Running lx.lionixCRM.checkEnvironment() function');
-                console.groupEnd();
-                lx.lionixCRM.checkEnvironment();
-            }
+            console.log("Check environment '%s' '%s' '%s' '%s'", 'all modules', 'lx-check-crm-environment.js', '!function()', 'observer');
+            console.log('Running lx.lionixCRM.checkEnvironment() function');
+            lx.lionixCRM.getEnvironment();
             // if needed only once, you can stop observing with observer.disconnect();
             observer.disconnect();
         }
-        // When need to find something special
-        // mutations.forEach(function(mutation) {
-        //     console.log('select2-lionixcrm mutation:', mutation);
-        //     //     if (mutation.type == "attributes") {
-        //     //         if (mutation.target.nodeName == "FORM" && mutation.target.id == "some-form-id") {
-        //     //             //when found do your code
-        //     //             //your code
-        //     //             // if needed only once, you can stop observing with observer.disconnect();
-        //     //             //observer.disconnect();
-        //     //         }
-        //     //     }
-        // });
     });
     // Observer target
     var target = document.querySelector('#content');
