3e1e66cf71700155b1c93af2bf88fe9ffcc749dd
Cristian Umaña <cumanacr@gmail.com>
2017-10-06 16:45:59 -0600

    refactor: upload folder created

--- custom/themes/SuiteP/js/style.js
+++ custom/themes/SuiteP/js/style.js
@@ -32,7 +32,7 @@ lx.lionixCRM.scripts.push("custom/lionix/js/jquery-plugins/fn.lxtest.js");
 lx.lionixCRM.scripts.push("custom/lionix/js/jquery-plugins/jquery.form.js");
 lx.lionixCRM.scripts.push("custom/lionix/js/jquery-plugins/toastr.js");
 lx.lionixCRM.scripts.push("custom/lionix/js/miscellaneous/lx-check-crm-environment.js");
-lx.lionixCRM.scripts.push("custom/lionix/js/miscellaneous/lx-upload-files-in-editview.js");
+lx.lionixCRM.scripts.push("custom/lionix/js/miscellaneous/upload/lx-upload-files-in-editview.js");
 lx.lionixCRM.scripts.push("custom/lionix/js/business-logic/notes/lx-remove-contacts-from-related-to.js");
 lx.lionixCRM.scripts.push("custom/lionix/js/business-logic/opportunities/lx-main-contact.js");
 lx.lionixCRM.scripts.push("custom/lionix/js/business-logic/opportunities/lx-account-name.js");
