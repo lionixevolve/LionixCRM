30486b401f5820ce750244f3809e80faf64ad545
Cristian Umaña <cumanacr@gmail.com>
2017-10-18 20:32:44 -0600

    feat: new main contact fields varderfs

---
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_maincontactduplicateid_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2017-10-18 13:57:44
+$dictionary['Opportunity']['fields']['maincontactduplicateid_c']['inline_edit']='';
+$dictionary['Opportunity']['fields']['maincontactduplicateid_c']['labelValue']='maincontactduplicateid';
+
+ ?>
---
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_maincontactphonemobile_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2017-10-18 12:27:16
+$dictionary['Opportunity']['fields']['maincontactphonemobile_c']['inline_edit']='1';
+$dictionary['Opportunity']['fields']['maincontactphonemobile_c']['labelValue']='Télefono celular nuevo contacto';
+
+ ?>
