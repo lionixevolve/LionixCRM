c0e3151d73741edd50ddd0f8a166ba15ccb8fdce
Cristian Umaña <cumanacr@gmail.com>
2018-04-02 13:22:29 -0600

    feat: pdo capabilities added

--- custom/lionix/lxdb/dbconn.php
+++ custom/lionix/lxdb/dbconn.php
@@ -33,9 +33,17 @@ class LxDBConnections
 
         $tempdb =& ADONewConnection($engine);
         $tempdb->NConnect($server, $user, $pass, $database);
+        $tempdb->setCharset('utf8');
         $tempdb->SetFetchMode(ADODB_FETCH_ASSOC);
-
         $this->db[$name] = $tempdb;
+
+        $mysql_cstr = "mysql:host={$server};dbname={$database}";
+        $cstr = $mysql_cstr;
+        $tempdb = new \PDO($cstr, $user, $pass);
+        $tempdb->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->db[$name.'pdo'] = $tempdb;
+        $query = "SET NAMES utf8";
+        $this->execute($query, $name.'pdo', 'pdo');
 
         // Create aditional adodb5 database connections defined on file config_override.php on SugarCRM root directory
         if (!empty($sugar_config['lx_adodb'])) {
@@ -49,40 +57,81 @@ class LxDBConnections
                 $database = $a['database'];
                 $active   = $a['active'];
                 $port     = empty($a['port']) ? "1521" : $a['port'];
+                $pdo      = $a['pdo'];
 
                 if ($active) {
-                    try {
-                        $tempdb =& ADONewConnection($engine);
-                        $tempdb->debug = false; //Only turn debug on testing, if leave it on while production ready json encodes fail.
-                        switch ($engine) {
-                        case "oci8":
-                            $oracle_cstr = "( DESCRIPTION = ( ADDRESS_LIST = ( ADDRESS = (PROTOCOL = TCP)(HOST = {$server})(PORT = {$port}) ) ) (CONNECT_DATA = (SERVICE_NAME = {$database}) ) )";
-                            $tempdb->NConnect($oracle_cstr, $user, $pass);
-                        break;
-                        default:
-                            $tempdb->NConnect($server, $user, $pass, $database);
-                            $tempdb->SetFetchMode(ADODB_FETCH_ASSOC);
-                    }
-                        $this->db[$name] = $tempdb;
-                    } catch (Exception $e) {
-                        error_log("Connection problem on custom/lionix/lxdb/dbconn.php line 43");
-                        error_log(print_r($a));
-                        error_log($e->getMessage().'<pre>'.$e->getTraceAsString().'</pre>');
-                    }//fin try
+                    if ($pdo) {
+                        $cstr = "";
+                        try {
+                            switch ($engine) {
+                                case "oracle":
+                                case "oci8":
+                                    $oracle_cstr = "( DESCRIPTION = ( ADDRESS_LIST = ( ADDRESS = (PROTOCOL = TCP)(HOST = {$server})(PORT = {$port}) ) ) (CONNECT_DATA = (SERVICE_NAME = {$database}) ) )";
+                                    $cstr = $oracle_cstr;
+                                break;
+                                case "postgresql":
+                                case "postgres9":
+                                    $postgresql_cstr = "pgsql:host={$server};port={$port};dbname={$database}";
+                                    $cstr = $postgresql_cstr;
+                                break;
+                                case "mysql":
+                                case "mysqli":
+                                    $mysql_cstr = "mysql:host={$server};dbname={$database}";
+                                    $cstr = $mysql_cstr;
+                                break;
+                            }
+                            $tempdb = new \PDO($cstr, $user, $pass);
+                            $tempdb->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                            $this->db[$name] = $tempdb;
+                        } catch (Exception $e) {
+                            error_log("Connection problem on custom/lionix/lxdb/dbconn.php line 43");
+                            error_log(print_r($a));
+                            error_log($e->getMessage().'<pre>'.$e->getTraceAsString().'</pre>');
+                        }
+                    } else {
+                        try {
+                            $tempdb =& ADONewConnection($engine);
+                            $tempdb->debug = false; //Only turn debug on testing, if leave it on while production ready json encodes fail.
+                            switch ($engine) {
+                                case "oci8":
+                                    $oracle_cstr = "( DESCRIPTION = ( ADDRESS_LIST = ( ADDRESS = (PROTOCOL = TCP)(HOST = {$server})(PORT = {$port}) ) ) (CONNECT_DATA = (SERVICE_NAME = {$database}) ) )";
+                                    $tempdb->NConnect($oracle_cstr, $user, $pass);
+                                break;
+                                default:
+                                    $tempdb->NConnect($server, $user, $pass, $database);
+                            }
+                            $tempdb->setCharset('utf8');
+                            $tempdb->setFetchMode(ADODB_FETCH_ASSOC);
+                            $this->db[$name] = $tempdb;
+                        } catch (Exception $e) {
+                            error_log("Connection problem on custom/lionix/lxdb/dbconn.php line 43");
+                            error_log(print_r($a));
+                            error_log($e->getMessage().'<pre>'.$e->getTraceAsString().'</pre>');
+                        }//fin try
+                    }//fin pdo
                 }//fin active
             }//fin foreach
         }//fin if
     }//fin function
 
-   public function execute($query, $connection_name = 'crm')
+   public function execute($query, $connection_name = 'crm', $connection_type = "adodb", $query_params = array())
    {
-       //Return adodb5 resultset after query execution
-       if ($connection_name != 'crm') {
-           error_log("Before run query for connection {$connection_name}");
+       if ($connection_type=='adodb') {
+           //Return adodb5 resultset after query execution
+           if ($connection_name != 'crm') {
+               error_log("Before run query for connection {$connection_name}");
+           }
+           error_log($query);
+           $answer = $this->db[$connection_name]->execute($query);
+           if ($connection_name != 'crm') {
+               error_log("After run query for connection {$connection_name}");
+           }
        }
-       $answer = $this->db[$connection_name]->execute($query);
-       if ($connection_name != 'crm') {
-           error_log("After run query for connection {$connection_name}");
+       if ($connection_type=='pdo') {
+           $stmt = $this->db[$connection_name]->prepare($query);
+           $stmt->setFetchMode(\PDO::FETCH_ASSOC);
+           $stmt->execute($query_params);
+           $answer = $stmt;
        }
        return $answer;
    }
