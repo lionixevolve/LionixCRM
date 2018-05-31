e1ddd431ef9cfa28353e7e8d9df1e1cdb9cfeb13
Cristian Umaña <cumanacr@gmail.com>
2018-05-31 11:26:49 -0600

    fix: missing file

--- lxajax_apps.php
+++ lxajax_apps.php
@@ -1,3 +1,4 @@
 <?php
+//Custom specific functions for each client
 class LxAJAXApps
 {}
--- lxajax_custom.php
+++ lxajax_custom.php
@@ -1,3 +1,5 @@
 <?php
+//Custom specific functions for each client
+require_once 'lxajax_apps.php';
 class LxAJAXCustom extends LxAJAXApps
 {}
