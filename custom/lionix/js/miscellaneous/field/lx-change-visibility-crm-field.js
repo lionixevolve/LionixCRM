7b9f8786b367b9fddf0c4d8edf2f7e90c323b322
Cristian Umaña <cumanacr@gmail.com>
2019-10-25 13:48:51 -0600

    fix: label wasn't hidden

--- custom/lionix/js/miscellaneous/field/lx-change-visibility-crm-field.js
+++ custom/lionix/js/miscellaneous/field/lx-change-visibility-crm-field.js
@@ -6,13 +6,13 @@ lx.field.visible = function(field_name, display, fnCallerName = "") {
     if (display) {
         console.log("lx.field.visible setting css visibility to visible on field " + field_name, fnCallerName);
         $("#" + field_name).css("visibility", "visible");
-        $("#" + field_name).parent("div").css("visibility", "visible");
+        $("#" + field_name).parent("div").parent("div").css("visibility", "visible");
         $(".label[data-label='LBL_" + field_name.toUpperCase() + "']").css("visibility", "visible");
         $(".label[data-label='LBL_" + label_field_name_without_c.toUpperCase() + "']").css("visibility", "visible");
     } else {
         console.log("lx.field.visible setting css visibility to hidden on field " + field_name, fnCallerName);
         $("#" + field_name).css("visibility", "hidden");
-        $("#" + field_name).parent("div").css("visibility", "hidden");
+        $("#" + field_name).parent("div").parent("div").css("visibility", "hidden");
         $(".label[data-label='LBL_" + field_name.toUpperCase() + "']").css("visibility", "hidden");
         $(".label[data-label='LBL_" + label_field_name_without_c.toUpperCase() + "']").css("visibility", "hidden");
     }
