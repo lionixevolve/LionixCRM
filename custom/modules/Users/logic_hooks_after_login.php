a831efd59b135ddd833268cfe47d0c99925005d8
Cristian Umaña <cumanacr@gmail.com>
2020-01-24 16:46:12 -0600

    feat: user_ip registered on tracker tablen on login and logout

--- custom/modules/Users/logic_hooks_after_login.php
+++ custom/modules/Users/logic_hooks_after_login.php
@@ -7,12 +7,89 @@ class LXUsersAfterLogInMethods
         global $current_user;
         $user_id = $_SESSION['authenticated_user_id'];
         $user_name = $current_user->user_name;
+        $user_ip = $this->get_ip_address();
         $session_id = session_id();
         $query = "
         INSERT INTO tracker
         (monitor_id, user_id, module_name, item_id, item_summary, date_modified, `action`, session_id, visible, deleted)
-        VALUES('login-logic-hook', '{$user_id}', 'Login', '{$user_id}', 'Log In performed by user {$user_name}', utc_timestamp(), 'login', '$session_id', 0, 0);
+        VALUES('login-logic-hook', '{$user_id}', 'Login', '{$user_id}', 'Log In performed by user {$user_name} on ip {$user_ip}', utc_timestamp(), 'login', '$session_id', 0, 0);
         ";
         $bean->db->query($query);
+    }
+
+    private function get_ip_address() {
+
+        // Check for shared Internet/ISP IP
+        if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        }
+
+        // Check for IP addresses passing through proxies
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+
+            // Check if multiple IP addresses exist in var
+            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') !== false) {
+                $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+                foreach ($iplist as $ip) {
+                    if ($this->validate_ip($ip))
+                        return $ip;
+                }
+            }
+            else {
+                if ($this->validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
+                    return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+        }
+        if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))
+            return $_SERVER['HTTP_X_FORWARDED'];
+        if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+            return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+            return $_SERVER['HTTP_FORWARDED_FOR'];
+        if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))
+            return $_SERVER['HTTP_FORWARDED'];
+
+        // Return unreliable IP address since all else failed
+        return $_SERVER['REMOTE_ADDR'];
+    }
+
+    /**
+     * Ensures an IP address is both a valid IP address and does not fall within
+     * a private network range.
+     */
+    private function validate_ip($ip) {
+
+        if (strtolower($ip) === 'unknown')
+            return false;
+
+        // Generate IPv4 network address
+        $ip = ip2long($ip);
+
+        // If the IP address is set and not equivalent to 255.255.255.255
+        if ($ip !== false && $ip !== -1) {
+            // Make sure to get unsigned long representation of IP address
+            // due to discrepancies between 32 and 64 bit OSes and
+            // signed numbers (ints default to signed in PHP)
+            $ip = sprintf('%u', $ip);
+
+            // Do private network range checking
+            if ($ip >= 0 && $ip <= 50331647)
+                return false;
+            if ($ip >= 167772160 && $ip <= 184549375)
+                return false;
+            if ($ip >= 2130706432 && $ip <= 2147483647)
+                return false;
+            if ($ip >= 2851995648 && $ip <= 2852061183)
+                return false;
+            if ($ip >= 2886729728 && $ip <= 2887778303)
+                return false;
+            if ($ip >= 3221225984 && $ip <= 3221226239)
+                return false;
+            if ($ip >= 3232235520 && $ip <= 3232301055)
+                return false;
+            if ($ip >= 4294967040)
+                return false;
+        }
+        return true;
     }
 }
