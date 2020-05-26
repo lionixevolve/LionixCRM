90d5dbcfafa8b7c1285cc6f54ae5fcfc7be5cb4b
Cristian Umaña <cumanacr@gmail.com>
2020-05-25 20:39:17 -0600

    refactor: lx.contact.getAccountNameByBusinessType as an async function [ch225]

--- custom/lionix/js/business-logic/contacts/lx-account-name.js
+++ custom/lionix/js/business-logic/contacts/lx-account-name.js
@@ -1,55 +1,87 @@
 //  function definitions section
-lx.contact.getAccountNameByBusinessType = function(forceCheck) {
+lx.contact.getAccountNameByBusinessType = async function (forceCheck) {
     try {
         if (forceCheck) {
             lx.lionixCRM.config.business_type = undefined;
             $("#account_name_lxajaxed").remove();
         }
         data = lx.lionixCRM.config.business_type.toLowerCase();
-        if ($("#account_name_lxajaxed").length == 0 || $('#account_name_lxajaxed').data('business_type') != data) {
+        if (
+            $("#account_name_lxajaxed").length == 0 ||
+            $("#account_name_lxajaxed").data("business_type") != data
+        ) {
             switch (data) {
-                case 'b2c':
-                    lx.field.validate('EditView', 'account_name', 'Nombre de Cuenta', false);
-                    lx.field.show('account_name', false);
+                case "b2c":
+                    lx.field.validate(
+                        "EditView",
+                        "account_name",
+                        "Nombre de Cuenta",
+                        false
+                    );
+                    lx.field.show("account_name", false);
                     break;
-                case 'b2b':
-                    lx.field.validate('EditView', 'account_name', 'Nombre de Cuenta', false);
-                    lx.field.show('account_name', true);
+                case "b2b":
+                    lx.field.validate(
+                        "EditView",
+                        "account_name",
+                        "Nombre de Cuenta",
+                        false
+                    );
+                    lx.field.show("account_name", true);
                     break;
             }
-            $('#account_name_lxajaxed').remove();
-            $("#account_name").append('<div id="account_name_lxajaxed" data-business_type="' + data + '" />');
+            $("#account_name_lxajaxed").remove();
+            $("#account_name").append(
+                '<div id="account_name_lxajaxed" data-business_type="' +
+                    data +
+                    '" />'
+            );
             console.warn("account_name_lxajaxed div indicator added.");
         } else {
             if (lx.lionixCRM.config.debuglx) {
-                console.warn('account_name_lxajaxed div indicator already exists:', $('#account_name_lxajaxed').data('business_type'));
+                console.warn(
+                    "account_name_lxajaxed div indicator already exists:",
+                    $("#account_name_lxajaxed").data("business_type")
+                );
             }
         }
     } catch (error) {
-        console.warn('business_type property is not present!');
-        console.warn('Retrieving business_type property...');
-        lx.lionixCRM.getConfigOption('business_type').then(function(data) {
-            console.warn('business_type successfully retrieved', data);
-            lx.contact.getAccountNameByBusinessType(false);
-        });
+        console.error(
+            "LionixCRM catched on lx.contact.getAccountNameByBusinessType 👇🏻"
+        );
+        console.error(error);
+        console.error("business_type property is not present!");
+        console.error("Retrieving business_type property...");
+        data = await lx.lionixCRM.getConfigOption("business_type");
+        console.error(
+            "business_type successfully retrieved on lx.contact.getAccountNameByBusinessType catch",
+            data
+        );
+        lx.contact.getAccountNameByBusinessType(false);
     }
-} // end function
+}; // end function
 
 //Self-Invoking Anonymous Function Notation
 // !function(){}();  easy to read, the result is unimportant.
 // (function(){})();  like above but more parens.
 // (function(){}());  Douglas Crockford's style when you need function results.
 // Further reading: http://javascript.crockford.com/code.html then search for invoked immediately
-!function() {
+!(function () {
     // create an observer instance
     // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
-    var observer = new MutationObserver(function(mutations) {
+    var observer = new MutationObserver(function (mutations) {
         if (mutations) {
-            var crmEditView = document.forms['EditView'];
+            var crmEditView = document.forms["EditView"];
             if (crmEditView && !!crmEditView.module) {
-                if (crmEditView.module.value == 'Contacts') {
+                if (crmEditView.module.value == "Contacts") {
                     if (lx.lionixCRM.config.debuglx) {
-                        console.warn("Bussines logic observer '%s' '%s' '%s' '%s'", 'contacts', 'lx-account-name.js', '!function()', 'initial');
+                        console.warn(
+                            "Bussines logic observer '%s' '%s' '%s' '%s'",
+                            "contacts",
+                            "lx-account-name.js",
+                            "!function()",
+                            "initial"
+                        );
                     }
                     lx.contact.getAccountNameByBusinessType(false);
                 }
@@ -59,7 +91,7 @@ lx.contact.getAccountNameByBusinessType = function(forceCheck) {
         }
     });
     // Observer target
-    var target = document.querySelector('#content');
+    var target = document.querySelector("#content");
     if (target) {
         // configuration of the observer:
         // NOTE: At the very least, childList, attributes, or characterData must be set to true. Otherwise, "An invalid or illegal string was specified" error is thrown.
@@ -67,11 +99,11 @@ lx.contact.getAccountNameByBusinessType = function(forceCheck) {
             attributes: true,
             childList: true,
             // characterData: true,
-            subtree: true
+            subtree: true,
         };
         // pass in the target node, as well as the observer options
         observer.observe(target, config);
     }
     // end observer
-}();
+})();
 //eof
