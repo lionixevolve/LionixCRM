1663edcc962292fbdda536d383a8e2696a1ea16b
Cristian Umaña <cumanacr@gmail.com>
2020-05-28 11:53:53 -0600

    refactor: lx.lionixCRM.getTSECRData as async function [ch225]

--- custom/lionix/js/miscellaneous/lionixcrm/lx-get-tse-cr-data.js
+++ custom/lionix/js/miscellaneous/lionixcrm/lx-get-tse-cr-data.js
@@ -1,66 +1,55 @@
 // function definitions section
-lx.lionixCRM.getTSECRData = function(searchObject) {
-    return new Promise(function(resolve, reject) {
-        // var method = "getContactDuplicates";
-        var method = "getTSEData";
-        var data = {
-            "method": method,
-            "cedula_c": searchObject.cedula_c
-        };
-        $.ajax({
-            // beforeSend is a pre-request callback function that can be used to modify the jqXHR.
-            beforeSend: function(jqXHR, settings) {
-                console.warn("Bussines logic '%s' '%s' '%s' '%s'", 'all modules', 'lx-get-tse-cr-data.js', 'lx.lionixCRM.getTSECRData', 'ajax beforeSend');
-                console.warn("beforeSend callback:", settings.url);
-            }, //end beforeSend
-            url: 'lxajax.php',
-            type: 'POST',
-            data: data,
-            // success is a function to be called if the request succeeds.
-            success: function(data, status, jqXHR) {
-                console.warn("Bussines logic '%s' '%s' '%s' '%s'", 'all modules', 'lx-get-tse-cr-data.js', 'lx.lionixCRM.getTSECRData', 'ajax success');
-                console.warn("success callback:", status);
-                console.warn("data:", data);
-                data = JSON.parse(data);
-                if (data.length == 0) {
-                    console.warn("TSE Costa Rica entry NOT FOUND:", searchObject.cedula_c);
-                    data = [
-                        {
-                            cedula_c: searchObject.cedula_c,
-                            first_name: "",
-                            last_name: "",
-                            lastname2_c: "",
-                            found: false
-                        }
-                    ];
-                    toastr["warning"]("Cédula no registrada.", "Búsqueda por cédula...", {
-                        "positionClass": "toast-bottom-center",
-                        "showDuration": "0",
-                        "hideDuration": "0",
-                        "timeOut": "4000",
-                        "extendedTimeOut": "0",
-                        "progressBar": true,
-                        "onShown": function() {
-                            console.warn('Focus on ' + searchObject.focusfieldname + 'after toastr.');
-                            $('#' + searchObject.focusfieldname).focus();
-                        }
-                    });
-                } else {
-                    console.warn("TSE Costa Rica entry found:", data.length, data);
-                }
-                resolve({"focusfieldname": searchObject.focusfieldname, "data": data});
-            }, // end success
-            // error is a function to be called if the request fails.
-            error: function(jqXHR, status, error) {
-                console.error("Bussines logic '%s' '%s' '%s' '%s'", 'all modules', 'lx-get-tse-cr-data.js', 'lx.lionixCRM.getTSECRData', 'ajax error');
-                console.error("error callback:", status);
-                console.error("Function lx.lionixCRM.getConfigOption error:", error);
-                reject(error);
-            }, // end error
-            // complete is a function to be called when the request finishes (after success and error callbacks are executed).
-            // complete: function(jqXHR, status) {
-            // },  end complete
-            datatype: "text"
-        }); // end ajax
+lx.lionixCRM.getTSECRData = async function (searchObject) {
+    let lxajax_method = "getTSEData";
+    let data = {
+        method: lxajax_method,
+    };
+    for (const databaseField in searchObject) {
+        if (searchObject.hasOwnProperty(databaseField)) {
+            if (databaseField != "fieldname") {
+                data[databaseField] = searchObject[databaseField];
+            }
+        }
+    }
+    let response = await fetch("lxajax.php", {
+        method: "POST",
+        body: new URLSearchParams(data),
+        headers: new Headers({
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        }),
     });
-} // end function
+    data = await response.json().catch((error) => {
+        console.error("Function lx.lionixCRM.getTSECRData error:", error);
+        return error;
+    });
+    if (data.length == 0) {
+        console.warn("TSE Costa Rica entry NOT FOUND:", searchObject.cedula_c);
+        data = [
+            {
+                cedula_c: searchObject.cedula_c,
+                first_name: "",
+                last_name: "",
+                lastname2_c: "",
+                found: false,
+            },
+        ];
+        toastr["warning"]("Cédula no registrada.", "Búsqueda por cédula...", {
+            positionClass: "toast-bottom-center",
+            showDuration: "0",
+            hideDuration: "0",
+            timeOut: "4000",
+            extendedTimeOut: "0",
+            progressBar: true,
+            onShown: function () {
+                console.warn(`Focus on ${searchObject.focusfieldname}`);
+                $("#" + searchObject.focusfieldname).focus();
+            },
+        });
+    } else {
+        console.warn("TSE Costa Rica entry found:", data.length, data);
+    }
+    return {
+        focusfieldname: searchObject.focusfieldname,
+        data: data,
+    };
+};
