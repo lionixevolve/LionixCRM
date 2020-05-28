5abb1524a50124e0f7d282eb8243cb00691a5d97
Cristian Umaña <cumanacr@gmail.com>
2020-05-28 16:26:26 -0600

    refactor: lx.lionixCRM.getEnvironment as async function [ch225]

--- custom/lionix/js/miscellaneous/lionixcrm/lx-check-crm-environment.js
+++ custom/lionix/js/miscellaneous/lionixcrm/lx-check-crm-environment.js
@@ -1,32 +1,39 @@
 // function definitions section
-lx.lionixCRM.getEnvironment = function(forceCheck) {
+lx.lionixCRM.getEnvironment = async function (forceCheck) {
     try {
         if (forceCheck) {
             lx.lionixCRM.config.environment = undefined;
             $("#LionixCRM-environment").remove();
         }
         if ($("#LionixCRM-environment").length == 0) {
-            if (lx.lionixCRM.config.environment.toLowerCase() === 'testing') {
+            if (lx.lionixCRM.config.environment.toLowerCase() === "testing") {
                 let pbclass = "progress-bar bg-info";
                 let probability = 100;
-                let text_bar = "Testing Environment"
-                $('#content').before('<div id="LionixCRM-environment" data-enviroment="Testing Environment" class="progresslx"> <div class="' + pbclass + '" role="progressbar" aria-valuenow="' + probability + '" aria-valuemin="0" aria-valuemax="100" style="min-width: 10%; width: ' + probability + '%; height: 30px; font-size:1.9em; line-height: 2.8rem;">' + text_bar + '</div></div>');
+                let text_bar = "Testing Environment";
+                $("#content").before(
+                    `<div id="LionixCRM-environment" data-enviroment="Testing Environment" class="progresslx">
+                        <div class="${pbclass}" role="progressbar" aria-valuenow="${probability}" aria-valuemin="0" aria-valuemax="100"
+                            style="min-width: 10%; width: ${probability}%; height: 30px; font-size:1.9em; line-height: 2.8rem;">${text_bar}
+                        </div>
+                    </div>`
+                );
             } else {
-                $('#content').before('<div id="LionixCRM-environment" data-enviroment="Production Environment" />');
+                $("#content").before(
+                    '<div id="LionixCRM-environment" data-enviroment="Production Environment" />'
+                );
             }
             console.warn("LionixCRM-environment div indicator added.");
         } else {
             console.warn("LionixCRM-environment div indicator already exists.");
         }
     } catch (error) {
-        console.error('Environment property is not present!');
-        console.error('Retrieving environment property...');
-        lx.lionixCRM.getConfigOption('environment').then(function(data) {
-            console.warn('Environment successfully retrieved', data);
-            lx.lionixCRM.getEnvironment(false);
-        });
+        console.error("Environment property is not present!");
+        console.error("Retrieving environment property...");
+        data = await lx.lionixCRM.getConfigOption("environment");
+        console.error("Environment successfully retrieved", data);
+        lx.lionixCRM.getEnvironment(false);
     }
-} // end function
+}; // end function
 
 // This file containts all modules bussines logic
 //Self-Invoking Anonymous Function Notation
@@ -37,16 +44,21 @@ lx.lionixCRM.getEnvironment = function(forceCheck) {
 !(function () {
     // create an observer instance
     // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
-    var observer = new MutationObserver(function(mutations) {
+    var observer = new MutationObserver(function (mutations) {
         if (mutations) {
-            console.warn("Check environment observer '%s' '%s' '%s'", 'all modules', 'lx-check-crm-environment.js', '!function()');
+            console.warn(
+                "Check environment observer '%s' '%s' '%s'",
+                "environment",
+                "lx-check-crm-environment.js",
+                "!function()"
+            );
             lx.lionixCRM.getEnvironment(false);
             // if needed only once, you can stop observing with observer.disconnect();
             observer.disconnect();
         }
     });
     // Observer target
-    var target = document.querySelector('#content');
+    var target = document.querySelector("#content");
     if (target) {
         // configuration of the observer:
         // NOTE: At the very least, childList, attributes, or characterData must be set to true. Otherwise, "An invalid or illegal string was specified" error is thrown.
@@ -54,11 +66,11 @@ lx.lionixCRM.getEnvironment = function(forceCheck) {
             attributes: true,
             childList: true,
             // characterData: true,
-            subtree: true
+            subtree: true,
         };
         // pass in the target node, as well as the observer options
         observer.observe(target, config);
     }
     // end observer
-}();
+})();
 //eof
