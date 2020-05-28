595ece3e82a75769ae8c65340d804cf462543cd5
Cristian Umaña <cumanacr@gmail.com>
2020-05-28 16:27:17 -0600

    refactor: lx-load-all-config-options.js [ch225]

--- custom/lionix/js/miscellaneous/lionixcrm/lx-load-all-config-options.js
+++ custom/lionix/js/miscellaneous/lionixcrm/lx-load-all-config-options.js
@@ -4,22 +4,30 @@
 // (function(){})(); // like above but more parens.
 // (function(){}()); // Douglas Crockford's style when you need function results.
 // Further reading: http://javascript.crockford.com/code.html and look for invoked immediately
-!function() {
+!(function () {
     // create an observer instance
     // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
-    var observer = new MutationObserver(function(mutations) {
+    var observer = new MutationObserver(function (mutations) {
         if (mutations) {
-            if (Object.keys(lx.lionixCRM.config).length === 0 && lx.lionixCRM.config.constructor === Object) {
-                console.warn("Check environment observer '%s' '%s' '%s'", 'all modules', 'lx-lionixcrm-load-config-options.js', '!function()');
-                console.warn('Loading all LionixCRM config options...');
-                lx.lionixCRM.getConfigOption('all');
+            if (
+                Object.keys(lx.lionixCRM.config).length === 0 &&
+                lx.lionixCRM.config.constructor === Object
+            ) {
+                console.warn(
+                    "Check environment observer '%s' '%s' '%s'",
+                    "all modules",
+                    "lx-lionixcrm-load-config-options.js",
+                    "!function()"
+                );
+                console.warn("Loading all LionixCRM config options...");
+                lx.lionixCRM.getConfigOption("all");
             }
             // if needed only once, you can stop observing with observer.disconnect();
             observer.disconnect();
         }
     });
     // Observer target
-    var target = document.querySelector('#content');
+    var target = document.querySelector("#content");
     if (target) {
         // configuration of the observer:
         // NOTE: At the very least, childList, attributes, or characterData must be set to true. Otherwise, "An invalid or illegal string was specified" error is thrown.
@@ -27,11 +35,11 @@
             attributes: true,
             childList: true,
             // characterData: true,
-            subtree: true
+            subtree: true,
         };
         // pass in the target node, as well as the observer options
         observer.observe(target, config);
     }
     // end observer
-}();
+})();
 //eof
